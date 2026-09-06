4f61fc3c0db88b660424f2239c4cee757f592b86
mostafa wael <85419817+mostafawael4@users.noreply.github.com>
2026-09-06 15:36:17 +0300

    Update production API URL

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bakr-backend-production-6c3a.up.railway.app/api',
+  apiUrl: 'https://bakr-backend-production-7251.up.railway.app/api',
 };
